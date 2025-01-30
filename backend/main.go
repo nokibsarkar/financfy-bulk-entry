3fd7b1f374e6dbc7ade618899011fad76171bb44
nokibsarkar <nokibsarkar@gmail.com>
2025-01-30 17:36:45 +0600

    integrated transaction

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"log"
-
 	"financify/bulk-entry/routes"
+	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
 	routes.Orchestrate(*r.Group("/"))
 
 	defer log.Println("Exiting...")
--- frontend/components/transaction/TransactionForm.tsx
+++ frontend/components/transaction/TransactionForm.tsx
@@ -1,147 +1,183 @@
 "use client"
-import { useCallback, useState } from "react";
-const AddTransactionForm = ({type} : {type : string}) => {
-    
-    const [date, setDate] = useState('');
-    const [voucherNo, setVoucherNo] = useState('');
-    const [amount, setAmount] = useState('');
-    const [contact, setContact] = useState('');
-    const [reference, setReference] = useState('');
-    const [remarks, setRemarks] = useState('');
-    const [category, setCategory] = useState('');
-    const [receiveMode, setReceiveMode] = useState('');
-    const saveHandler = useCallback((e) => {
-        const data = {
-            date,
-            voucherNo,
-            amount,
-            contact,
-            reference,
-            remarks,
-            category,
-            receiveMode
-        };
-        console.log(data);
-    }, [])
+import { BaseURL } from "@/consts/server";
+import { ResponseMultiple } from "@/types/response";
+import { useState } from "react";
 
-    return (
-      <form onSubmit={e => e.preventDefault()}>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <label className="block text-sm font-medium text-black">
-              Date & time *
-            </label>
-            <input
-              type="datetime-local"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md text-xs text-gray-700"
-              value={date}
-              onChange={e => setDate(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label className="block text-sm font-medium text-black">
-              Voucher number *
-            </label>
-            <input
-              type="text"
-              defaultValue="2"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-                value={voucherNo}
-                onChange={e => setVoucherNo(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label className="block text-sm font-medium text-black">
-              Amount *
-            </label>
-            <input
-              type="text"
-              placeholder="৳ 0.00"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label className="block text-sm font-medium text-black">
-              Contact
-            </label>
-            <select className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-                value={contact}
-                onChange={e => setContact(e.target.value)}
-            >
-              <option>Contact</option>
-              <option>Mst. Rukaiya Islam Tonni</option>
-            </select>
-          </div>
-  
-          <div>
-            <label className="block text-sm font-medium  text-black">
-              Receive mode *
-            </label>
-            <select className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-                value={receiveMode}
-                onChange={e => setReceiveMode(e.target.value)}
-            >
-              <option>Receive mode</option>
-              <option>Cash</option>
-              <option>Bank</option>
-              <option>Online method</option>
-            </select>
-          </div>
-  
-          <div>
-            <label className="block text-sm font-medium  text-black">
-              Category *
-            </label>
-            <select
-            value={category}
-            onChange={e => setCategory(e.target.value)}
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs">
-              <option>Category</option>
-              <option>Salary</option>
-            </select>
-          </div>
-        </div>
-  
-        <div className="mt-4">
-          <label className="block text-sm font-medium  text-black">
-            Reference
-          </label>
-          <input
-            type="text"
-            value={reference}
-            onChange={e => setReference(e.target.value)}
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-          />
-        </div>
-  
-        <div className="mt-4">
-          <label className="block text-sm font-medium  text-black">
-            Remarks
-          </label>
-          <textarea
-            value={remarks} 
-            onChange={e => setRemarks(e.target.value)}
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
-            rows={3}
-          ></textarea>
-        </div>
-  
-  
-        <div className="mt-6">
-          <button
-            type="submit"
-            className="w-full bg-primary text-white py-2 px-4 rounded-md "
-            onClick={saveHandler}
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    )
-  }
-  export default AddTransactionForm;
+type AddTransactionFormProps = {
+	type: string;
+	date: string;
+	voucherNo: string;
+	amount: number;
+	contact: string;
+	reference: string;
+	remarks: string;
+	category: string;
+	receiveMode: string;
+	cashbookId: number;
+}
+const AddTransactionForm = ({ type, cashbookId = 1 }: { type: string, cashbookId: number }) => {
+
+	const [date, setDate] = useState('');
+	const [voucherNo, setVoucherNo] = useState('');
+	const [amount, setAmount] = useState('');
+	const [contact, setContact] = useState('');
+	const [reference, setReference] = useState('');
+	const [remarks, setRemarks] = useState<string>('');
+	const [category, setCategory] = useState<string>('');
+	const [receiveMode, setReceiveMode] = useState('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const saveHandler = (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		const data: AddTransactionFormProps = {
+			"voucherNo": voucherNo,
+			"date": date,
+			"category": category,
+			"type": type,
+			"amount": Number(amount),
+			"cashbookId": cashbookId,
+			"receiveMode": receiveMode,
+			"contact": contact,
+			"reference": reference,
+			"remarks": remarks
+		}
+		const url = BaseURL + '/api/v1/transaction/';
+		setLoading(true);
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		}).then(a => a.json()).then((a : ResponseMultiple<{[k : string] : any}>) => {
+			if(!a.error) {
+				alert('Transaction saved successfully');
+			} else {
+				throw new Error(a.error);
+			}
+		}).catch(a => {
+			console.log(a);
+		}).finally(() => {
+			setLoading(false);
+		});
+	};
+
+	return (
+		<form onSubmit={e => e.preventDefault()}>
+			<div className="grid grid-cols-2 gap-4">
+				<div>
+					<label className="block text-sm font-medium text-black">
+						Date & time *
+					</label>
+					<input
+						type="datetime-local"
+						className="w-full mt-1 p-2 border border-gray-300 rounded-md text-xs text-gray-700"
+						value={date}
+						onChange={e => setDate(e.target.value)}
+					/>
+				</div>
+
+				<div>
+					<label className="block text-sm font-medium text-black">
+						Voucher number *
+					</label>
+					<input
+						type="text"
+						className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+						value={voucherNo}
+						onChange={e => setVoucherNo(e.target.value)}
+					/>
+				</div>
+
+				<div>
+					<label className="block text-sm font-medium text-black">
+						Amount *
+					</label>
+					<input
+						type="text"
+						placeholder="৳ 0.00"
+						className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+						value={amount}
+						onChange={e => setAmount(e.target.value)}
+					/>
+				</div>
+
+				<div>
+					<label className="block text-sm font-medium text-black">
+						Contact
+					</label>
+					<select className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+						value={contact}
+						onChange={e => setContact(e.target.value)}
+					>
+						<option>Contact</option>
+						<option>Mst. Rukaiya Islam Tonni</option>
+					</select>
+				</div>
+
+				<div>
+					<label className="block text-sm font-medium  text-black">
+						Receive mode *
+					</label>
+					<select className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+						value={receiveMode}
+						onChange={e => setReceiveMode(e.target.value)}
+					>
+						<option>Receive mode</option>
+						<option>Cash</option>
+						<option>Bank</option>
+						<option>Online method</option>
+					</select>
+				</div>
+
+				<div>
+					<label className="block text-sm font-medium  text-black">
+						Category *
+					</label>
+					<select
+						value={category}
+						onChange={e => setCategory(e.target.value)}
+						className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs">
+						<option>Category</option>
+						<option>Salary</option>
+					</select>
+				</div>
+			</div>
+
+			<div className="mt-4">
+				<label className="block text-sm font-medium  text-black">
+					Reference
+				</label>
+				<input
+					type="text"
+					value={reference}
+					onChange={e => setReference(e.target.value)}
+					className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+				/>
+			</div>
+
+			<div className="mt-4">
+				<label className="block text-sm font-medium  text-black">
+					Remarks
+				</label>
+				<textarea
+					value={remarks}
+					onChange={e => setRemarks(e.target.value)}
+					className="w-full mt-1 p-2 border border-gray-300 rounded-md text-gray-700 text-xs"
+					rows={3}
+				></textarea>
+			</div>
+
+
+			<div className="mt-6">
+				<button
+					type="submit"
+					className="w-full bg-primary text-white py-2 px-4 rounded-md "
+					onClick={saveHandler}
+					disabled={loading}
+				>
+					Save
+				</button>
+			</div>
+		</form>
+	)
+}
+export default AddTransactionForm;
