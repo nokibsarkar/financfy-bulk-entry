368e5767ff4923025c8a39c090e912631a0990f4
--global <--global>
2025-01-31 10:56:42 +0600

    added placejolder

--- frontend/app/cashFlow/[id]/page.tsx
+++ frontend/app/cashFlow/[id]/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 export default function transaction({params} : {params : {id : string}}){
     const {id}= params
     return(
-        
+        <p>
+            This is the transactions of that specific cashflow
+        </p>
     )
 }
